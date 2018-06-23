8bcb3a5a78fad2adb91c61016ff3dd38d1b2b4da
iwilliams83 <indirajamurath@gmail.com>
2018-06-23 22:55:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,8 @@ class Application
       item = @@items.find{|i| i.name == item}
       resp.write item.price
     else
-      
-      return 404
+
+      resp.write 404
     end
 
     resp.finish
