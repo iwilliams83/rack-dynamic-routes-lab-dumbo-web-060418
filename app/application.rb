1906a655d9f1eb4a3a6c7f0dc57a0c578d869037
iwilliams83 <indirajamurath@gmail.com>
2018-06-23 23:49:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,14 +9,10 @@ class Application
     if req.path.match(/items/)
       item = req.path.split("/items/").last
 
-      @@items.each do |i|
-        puts "i = #{i}"
-        if i.name == item
+        if @@items.include?(Item.find_by(name: item))
           resp.write i.price
           resp.status = 200
-          return
         else
-          puts "item #{item} was not found"
           resp.write "Item not found"
           resp.status = 400
         end
