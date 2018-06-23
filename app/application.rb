09145d360fad597410a377ca146009ebe4ddb563
iwilliams83 <indirajamurath@gmail.com>
2018-06-23 23:28:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,10 +12,11 @@ class Application
       @@items.each do |i|
         puts "i = #{i}"
         if i.name == item
-          puts "i.price = #{i.price}"
+          puts "item found and price = #{i.price}"
           resp.write i.price
           resp.status = 200
         else
+          puts "item #{item} was not found"
           resp.write "Item not found"
           resp.status = 400
         end
