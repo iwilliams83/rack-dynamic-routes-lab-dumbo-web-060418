fedaa1af43d3f8fc230837eb2ed4b5c6cbfea968
iwilliams83 <indirajamurath@gmail.com>
2018-06-23 23:06:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      if @@items.include?(item)
+      if @@item.include?(item)
         item = @@items.find{|i| i.name == item}
         resp.write item.price
       # else
