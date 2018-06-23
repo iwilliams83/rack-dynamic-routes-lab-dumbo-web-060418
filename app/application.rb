16826b0d5beb1d34817755fd4aaf172ddbaa0026
iwilliams83 <indirajamurath@gmail.com>
2018-06-23 22:59:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,13 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item}
-      resp.write item.price
+      if @@items.include?(item) == false 
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        item = @@items.find{|i| i.name == item}
+        resp.write item.price
+      end
     else
       resp.write "Route not found"
       resp.status = 404
